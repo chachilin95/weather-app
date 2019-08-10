d571a333fe2f77787f40b40452d698b815359984
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-08-10 11:32:12 -0500

    Created reusable forecast function with darksky

--- api/darksky.js
+++ api/darksky.js
@@ -1,12 +1,36 @@
 const fs = require('fs');
+const request = require('request');
 
 const apiKey = fs.readFileSync('./keys/darksky.secret.key').toString();
-const api = `https://api.darksky.net/forecast/${apiKey}`;
 
-const generateURL = () => {
-    return api + '/37.8267,-122.4233'
+const generateURL = ({ latitude, longitude }) =>  (
+    `https://api.darksky.net/forecast/${apiKey}/${latitude},${longitude}`
+);
+
+const forecast = (location, callback) => {
+    const url = generateURL(location);
+
+    request({ url, json: true }, (error, response) => {    
+        
+        if (error) {
+            return callback('Unable to connect to weather service.', undefined);
+        } else if (error = response.body.error) {
+            return callback(error, undefined);
+        }
+
+        const { currently, daily } = response.body;
+        const temperature = currently.temperature;
+        const precipitation = currently.precipProbability;
+        const todaySummary = daily.data[0].summary;
+        
+        callback(
+            undefined,
+            `${todaySummary} It is currently ${temperature} degrees out. There is a ${precipitation}% chance of rain`
+        );
+        
+    });
 };
 
 module.exports = {
-    generateURL
+    forecast
 };
