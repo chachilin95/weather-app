1e803f36d3652b5e310bed137aba4c0f791430a5
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-08-10 10:23:19 -0500

    added error handling to api requests

--- app.js
+++ app.js
@@ -2,30 +2,36 @@ const request = require('request');
 const DarkSky = require('./api/darksky');
 const MapBox = require('./api/mapbox');
 
-const mbURL = MapBox.generateURL();
-const dsURL = DarkSky.generateURL();
+const weatherURL = DarkSky.generateURL();
+const geocodingURL = MapBox.generateURL();
 
-request({ url: dsURL, json: true }, (error, response) => {
+request({ url: weatherURL, json: true }, (error, response) => {
 
     if (error) {
+        console.log('Unable to connect to weather service.');
+    } else if (error = response.body.error) {
         console.log(error);
-        return;
+    } else {
+        const { currently, daily } = response.body;
+        const temperature = currently.temperature;
+        const precipitation = currently.precipProbability;
+        const todaySummary = daily.data[0].summary;
+    
+        console.log(
+            todaySummary,
+            `It is currently ${temperature} degrees out. There is a ${precipitation}% chance of rain`
+        );
     }
-
-    const { currently, daily } = response.body;
-    const temperature = currently.temperature;
-    const precipitation = currently.precipProbability;
-    const todaySummary = daily.data[0].summary;
-
-    console.log(
-        todaySummary,
-        `It is currently ${temperature} degrees out. There is a ${precipitation}% chance of rain`
-    );
 });
 
-request({ url: mbURL, json: true }, (err, response) => {
-    const { features } = response.body;
-    const [ longitude, latitude ] = features[0].center;
+request({ url: geocodingURL, json: true }, (error, response) => {
 
-    console.log(latitude, longitude);
+    if (error) {
+        console.log('Unable to connect to location services.');
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location, try another search term');
+    } else {
+        const [ longitude, latitude ] = response.body.features[0].center;    
+        console.log(latitude, longitude);
+    }
 });
