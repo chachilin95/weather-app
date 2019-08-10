81cb022b34ca22c00107bf7241d87792195762fe
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-08-10 10:55:56 -0500

    Created reusable geocoding function

--- api/mapbox.js
+++ api/mapbox.js
@@ -1,12 +1,34 @@
 const fs = require('fs');
+const request = require('request');
 
 const apiKey = fs.readFileSync('./keys/mapbox.secret.key').toString();
-const api = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${apiKey}`;
 
-const generateURL = () => {
-    return api
+const generateURL = (place) => {
+    const placeURI = encodeURIComponent(place);
+    return `https://api.mapbox.com/geocoding/v5/mapbox.places/${placeURI}.json?access_token=${apiKey}`
+};
+
+const geocode = (place, callback) => {
+    const url = generateURL(place);
+
+    request({ url, json: true }, (error, response) => {
+
+        if (error) {
+            callback('Unable to connect to location services.', undefined);
+        } else if (response.body.features.length === 0) {
+            callback('Unable to find location, try another search term', undefined);
+        } else {
+            const [longitude, latitude] = response.body.features[0].center;
+            const location = response.body.features[0].place_name
+            callback(undefined, {
+                longitude,
+                latitude,
+                location
+            });
+        }
+    });
 };
 
 module.exports = {
-    generateURL
+    geocode
 };
--- app.js
+++ app.js
@@ -1,37 +1,7 @@
-const request = require('request');
-const DarkSky = require('./api/darksky');
-const MapBox = require('./api/mapbox');
+const geocode = require('./api/mapbox').geocode;
 
-const weatherURL = DarkSky.generateURL();
-const geocodingURL = MapBox.generateURL();
-
-request({ url: weatherURL, json: true }, (error, response) => {
-
-    if (error) {
-        console.log('Unable to connect to weather service.');
-    } else if (error = response.body.error) {
-        console.log(error);
-    } else {
-        const { currently, daily } = response.body;
-        const temperature = currently.temperature;
-        const precipitation = currently.precipProbability;
-        const todaySummary = daily.data[0].summary;
-    
-        console.log(
-            todaySummary,
-            `It is currently ${temperature} degrees out. There is a ${precipitation}% chance of rain`
-        );
-    }
-});
-
-request({ url: geocodingURL, json: true }, (error, response) => {
-
-    if (error) {
-        console.log('Unable to connect to location services.');
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location, try another search term');
-    } else {
-        const [ longitude, latitude ] = response.body.features[0].center;    
-        console.log(latitude, longitude);
-    }
+geocode('Dallas, TX', (error, data) => {
+    console.log('Error: ', error);
+    console.log('Data: ', data);
+    console.log(data.location);
 });
